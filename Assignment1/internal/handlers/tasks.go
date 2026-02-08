030a9799de53ea0f5167e7e8235e33f9732a3edb
tamirlanm <tamirlanmirgali@gmail.com>
2026-02-08 18:15:28 +0500

    ch

--- Assignment1/internal/handlers/tasks.go
+++ Assignment1/internal/handlers/tasks.go
@@ -1,1 +1,13 @@
+package handlers
 
+import (
+	"Assignment1/internal/storage"
+)
+
+type TaskHandler struct {
+	storage *storage.TaskStorage
+}
+
+func NewTaskHandler(storage *storage.TaskStorage) *TaskHandler {
+	return &TaskHandler{storage: storage}
+}
