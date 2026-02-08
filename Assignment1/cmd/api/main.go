996cff66b7947ed9b7ffb8a9c08dd08ba630434e
tamirlanm <tamirlanmirgali@gmail.com>
2026-02-08 18:16:38 +0500

    ch

--- Assignment1/cmd/api/main.go
+++ Assignment1/cmd/api/main.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"Assignment1/internal/handlers"
+	"Assignment1/internal/storage"
+)
+
+func main() {
+	store := storage.NewTaskStorage()
+
+	taskHandler := handlers.NewTaskHandler(store)
+
+}
