dea2597b3e7c0d068b259fcf9c116c94656d2007
tamirlanm <tamirlanmirgali@gmail.com>
2026-03-29 23:04:41 +0500

    problem1

--- Practice6/problem1_rwmutex.go
+++ Practice6/problem1_rwmutex.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	safeMap := make(map[string]int)
+	var mu sync.RWMutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(i)
+
+		go func(key int) {
+			mu.Lock()
+			safeMap["key"] = key
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+
+	mu.Unlock()
+	value := safeMap["key"]
+	mu.RUnlock()
+	fmt.Printf("Value: %d\n", value)
+}
--- Practice6/problem1_syncmap.go
+++ Practice6/problem1_syncmap.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var safeMap sync.Map
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func(key int) {
+			defer wg.Done()
+			safeMap.Store("key", key)
+		}(i)
+	}
+
+	wg.Wait()
+	value, ok := safeMap.Load("key")
+	if ok {
+		fmt.Printf("Value: %v\n", value)
+
+	} else {
+		fmt.Printf("Key not found")
+	}
+}
